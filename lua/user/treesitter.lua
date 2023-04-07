1ce843a6990493046a5157c8bcab99374836f3ed
Ubuntu <azureuser@vm1.n0htkotsqsje1iafuupgez43ac.hx.internal.cloudapp.net>
2023-04-07 06:40:13 +0000

    treesitter add go gomod gowork

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,114 +1,108 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-
-
-
-
-    incremental_selection = {
+ ensure_installed = { "bash", "go", "gomod", "gowork", "vim", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+--  ensure_installed = "all" , -- one of "all" or a list of languages
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = { "css" },        -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
+  incremental_selection = {
+    enable = enable,
+    keymaps = {
+      -- mappings for incremental selection (visual mappings)
+      init_selection = "gnn",      -- maps in normal mode to init the node/scope selection
+      node_incremental = "grn",    -- increment to the upper named parent
+      scope_incremental = "grc",   -- increment to the upper scope (as defined in locals.scm)
+      node_decremental = "grm"     -- decrement to the previous node
+    }
+  },
+  textobjects = {
+    -- syntax-aware textobjects
+    enable = enable,
+    lsp_interop = {
       enable = enable,
-      keymaps = {
-        -- mappings for incremental selection (visual mappings)
-        init_selection = "gnn", -- maps in normal mode to init the node/scope selection
-        node_incremental = "grn", -- increment to the upper named parent
-        scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
-        node_decremental = "grm" -- decrement to the previous node
+      peek_definition_code = {
+        ["DF"] = "@function.outer",
+        ["DF"] = "@class.outer"
       }
     },
-
-    textobjects = {
-      -- syntax-aware textobjects
-      enable = enable,
-      lsp_interop = {
-        enable = enable,
-        peek_definition_code = {
-          ["DF"] = "@function.outer",
-          ["DF"] = "@class.outer"
-        }
+    keymaps = {
+      ["iL"] = {
+        -- you can define your own textobjects directly here
+        go = "(function_definition) @function",
       },
+      -- or you use the queries from supported languages with textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["aC"] = "@class.outer",
+      ["iC"] = "@class.inner",
+      ["ac"] = "@conditional.outer",
+      ["ic"] = "@conditional.inner",
+      ["ae"] = "@block.outer",
+      ["ie"] = "@block.inner",
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+      ["is"] = "@statement.inner",
+      ["as"] = "@statement.outer",
+      ["ad"] = "@comment.outer",
+      ["am"] = "@call.outer",
+      ["im"] = "@call.inner"
+    },
+    move = {
+      enable = enable,
+      set_jumps = true,   -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer"
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer"
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer"
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer"
+      }
+    },
+    select = {
+      enable = enable,
       keymaps = {
-        ["iL"] = {
-          -- you can define your own textobjects directly here
-          go = "(function_definition) @function",
-        },
-        -- or you use the queries from supported languages with textobjects.scm
+        -- You can use the capture groups defined in textobjects.scm
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
-        ["aC"] = "@class.outer",
-        ["iC"] = "@class.inner",
-        ["ac"] = "@conditional.outer",
-        ["ic"] = "@conditional.inner",
-        ["ae"] = "@block.outer",
-        ["ie"] = "@block.inner",
-        ["al"] = "@loop.outer",
-        ["il"] = "@loop.inner",
-        ["is"] = "@statement.inner",
-        ["as"] = "@statement.outer",
-        ["ad"] = "@comment.outer",
-        ["am"] = "@call.outer",
-        ["im"] = "@call.inner"
-      },
-      move = {
-        enable = enable,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer"
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer"
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer"
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer"
-        }
-      },
-      select = {
-        enable = enable,
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          -- Or you can define your own textobjects like this
-          ["iF"] = {
-            python = "(function_definition) @function",
-            cpp = "(function_definition) @function",
-            c = "(function_definition) @function",
-            java = "(method_declaration) @function",
-            go = "(method_declaration) @function"
-          }
-        }
-      },
-      swap = {
-        enable = enable,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner"
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner"
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        -- Or you can define your own textobjects like this
+        ["iF"] = {
+          python = "(function_definition) @function",
+          cpp = "(function_definition) @function",
+          c = "(function_definition) @function",
+          java = "(method_declaration) @function",
+          go = "(method_declaration) @function"
         }
       }
+    },
+    swap = {
+      enable = enable,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner"
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner"
+      }
     }
-
-
+  }
 })
